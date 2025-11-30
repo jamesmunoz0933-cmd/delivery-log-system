534986ce046890ed4af0806db54d9c7ab0b9816c
jamesmunoz0933-cmd <jamesmunoz0933@gmail.com>
2025-11-30 22:35:06 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,34 @@
 const CACHE_NAME = 'delivery-log-cache-v1';
-
 const urlsToCache = [
-  './',
   'index.html',
-  'monitor.html',
   'style.css',
   'grab.html',
   'panda.html',
-  'admin.html',
   'admin-login.html',
+  'admin.html',
   'admin-dashboard.html',
-  'grab-login.html',
-  'panda-login.html',
-  'firebase.js',
-  'manifest.json',
-  'icons/web-app-manifest-192x192.png',
-  'icons/web-app-manifest-512x512.png'
+  'monitor.html',
+  'firebase.js'
 ];
 
-// Install SW and cache all files
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activate SW
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
     )
   );
 });
 
-// Fetch cached files
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
-
